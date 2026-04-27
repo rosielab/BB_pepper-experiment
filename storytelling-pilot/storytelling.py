15084e6606f7de147a76161d48d4d83ef2c13c65
Avneet Batra <avneetbatra36@gmail.com>
2026-04-27 14:26:56 -0700

    feat: added 3 more interactions

--- storytelling-pilot/storytelling.py
+++ storytelling-pilot/storytelling.py
@@ -345,26 +345,31 @@ if __name__ == "__main__":
         
         inserts = {
             2: "chime",
-            5: "chime",
+            6: "chime",
             10: "Where do you think the frog is?",
             11: "chime",
             14: "chime",
+            15: "Let's call for the frog together",
             16: "chime",
             19: "chime",
+            23: "What do you think is in the hollow",
             24: "chime",
             26: "chime",
             29: "chime",
             31: "Have you ever seen a deer before?",
             35: "chime",
             36: "chime",
-            49: "chime",
+            39: "What do you think they heard?",
+            40: "chime",
             41: "chime",
+            43: "What sounds do you think the frog makes?",
             44: "chime",
             47: "Do you think the frog will be happy with his family?",
         }
 
         with open(SCRIPT_PATH, 'r') as file:
             for i, line in enumerate(file):
+                print("Insert", i)
                 if i in inserts and inserts[i] != "chime":
                     spk = torch.tensor([12], device=tts_device, dtype=torch.long)
 
@@ -382,6 +387,7 @@ if __name__ == "__main__":
                     os.environ["ALSA_PCM_CARD"] = "0"
                     # wait for response to generate the next (record using ALSA directly; avoids PyAudio crash)
                     print("Now recording 🎤 (Press Enter to stop)")
+
                     import subprocess, signal
                     rec_cmd = ["arecord", "-D", "plughw:0,0", "-f", "S16_LE", "-r", "16000", "-c", "1", f"./results/output-{i}.wav"]
                     proc = subprocess.Popen(rec_cmd)
@@ -404,10 +410,36 @@ if __name__ == "__main__":
                     
                     #feedback
                     fb_id = f"fb-{i}"
-                    play_only_synthesis(
-                        tts_device, tts_model, vocoder, denoiser,
-                        "Oh, very interesting.", spk, LANGUAGE, fb_id
-                    )
+                    if inserts[i] == "Where do you think the frog is?":
+                        play_only_synthesis(
+                            tts_device, tts_model, vocoder, denoiser,
+                            "Let's go see!", spk, LANGUAGE, fb_id
+                        )
+                    if inserts[i] == "Have you ever seen a deer before?":
+                        play_only_synthesis(
+                            tts_device, tts_model, vocoder, denoiser,
+                            "That's interesting", spk, LANGUAGE, fb_id
+                        )
+                    if inserts[i] == "Let's call for the frog together":
+                        play_only_synthesis(
+                            tts_device, tts_model, vocoder, denoiser,
+                            "Sounds great!", spk, LANGUAGE, fb_id
+                        )
+                    if inserts[i] == "What do you think is in the hollow":
+                        play_only_synthesis(
+                            tts_device, tts_model, vocoder, denoiser,
+                            "It's an owl!", spk, LANGUAGE, fb_id
+                        )
+                    if inserts[i] == "What sounds do you think the frog makes?":
+                        play_only_synthesis(
+                            tts_device, tts_model, vocoder, denoiser,
+                            "I love it!", spk, LANGUAGE, fb_id
+                        ),
+                    if inserts[i] == "What do you think they heard?":
+                        play_only_synthesis(
+                            tts_device, tts_model, vocoder, denoiser,
+                            "Cool!", spk, LANGUAGE, fb_id
+                        )
 
                     fb_wav = f"{WAV_PATH}/to_play-{fb_id}.wav"
                     wait_done(fb_wav)
@@ -469,7 +501,7 @@ if __name__ == "__main__":
             fb_id = f"fb-{i+1}"
             play_only_synthesis(
                 tts_device, tts_model, vocoder, denoiser,
-                "Oh, very interesting.", spk, LANGUAGE, fb_id
+                "Great answer!", spk, LANGUAGE, fb_id
             )
             
             spk = torch.tensor([107], device=tts_device, dtype=torch.long)
